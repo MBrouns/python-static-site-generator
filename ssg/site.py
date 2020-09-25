d50a922b8ae6efd38a36690aecb179359a9817d4
MBrouns <matthijs.brouns@gmail.com>
2020-09-25 11:08:55 +0200

    eh?

--- ssg/site.py
+++ ssg/site.py
@@ -8,12 +8,10 @@ class Site:
 
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
-
         directory.mkdir(exist_ok=True, parents=True)
 
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
-
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
